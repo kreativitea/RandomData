43064d5c9c8f0a6cc8acc076ece04618ea4de675
mike <mike@elacarte.com>
2014-01-20 01:46:47 -0800

    Fixing location of templates

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
-ENDPOINT = '127.0.0.1'
-PORT = '8000'
-TEMPLATE = 'template.json'
+# Template settings
+TEMPLATE = 'templates/example.json'
 TEMPLATE_FORMAT = 'json'
 
+# HTTP settings
+ENDPOINT = '127.0.0.1'
+PORT = '8000'
 AUTH = None
 HTTP_HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 SSL_VERIFY = False
